29a51cf6bdf0d8f751040f2db32946d298450e7f
Deep L Sukhwani <deep@springboard.com>
2018-01-23 11:57:47 +0530

    Refactor code

--- learning_falcon/images.py
+++ learning_falcon/images.py
@@ -9,11 +9,10 @@ import os
 
 
 class Resource(object):
-    _CHUNK_SIZE_BYTES = 4096
 
     # The resource object must now be initialised with a path used during POST
-    def __init__(self, storage_path):
-        self._storage_path = storage_path
+    def __init__(self, image_store):
+        self._image_store = image_store
 
     def on_get(self, req, resp):
         doc = {
@@ -24,28 +23,39 @@ class Resource(object):
             ]
         }
 
-        # Create a JSON representation of the resource
         resp.data = msgpack.packb(doc, use_bin_type=True)
         resp.content_type = falcon.MEDIA_MSGPACK
-
-        # resp.body = json.dumps(doc, ensure_ascii=False)
 
         # The following can be omitted since 200 is the default status returned by the framework, but it is included
         # here to illustrate how this may be overridden as needed.
         resp.status = falcon.HTTP_200
 
     def on_post(self, req, resp):
-        ext = mimetypes.guess_extension(req.content_type)
-        name = '{uuid}{ext}'.format(uuid=uuid.uuid4(), ext=ext)
+        name = self._image_store.save(req.stream, req.content_type)
+        resp.status = falcon.HTTP_201
+        resp.location = '/images/' + name
+
+
+class ImageStore(object):
+    _CHUNK_SIZE_BYTES = 4096
+
+    # Note the use of dependency injection for standard library methods. We'll use these later to avoid monkey-patching.
+    def __init__(self, storage_path, uuidgen=uuid.uuid4, fopen=io.open):
+        self._storage_path = storage_path
+        self._uuidgen = uuidgen
+        self._fopen = fopen
+
+    def save(self, image_stream, image_content_type):
+        ext = mimetypes.guess_extension(image_content_type)
+        name = '{uuid}{ext}'.format(uuid=self._uuidgen(), ext=ext)
         image_path = os.path.join(self._storage_path, name)
 
-        with io.open(image_path, 'wb') as image_file:
+        with self._fopen(image_path, 'wb') as image_file:
             while True:
-                chunk = req.stream.read(self._CHUNK_SIZE_BYTES)
+                chunk = image_stream.read(self._CHUNK_SIZE_BYTES)
                 if not chunk:
                     break
 
                 image_file.write(chunk)
 
-        resp.status = falcon.HTTP_201
-        resp.location = '/images/' + name
+        return name
